df0c41a2dd8a53dc419189b6a9c717bf5fc46550
abhishek23027kushwaha <abhishek8888raj@gmail.com>
2026-01-09 00:25:49 +0530

    url changes

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin:"https://virtual-classes-2.onrender.com/" ||"http://localhost:5173", // frontend URL
     credentials: true,
   })
 );
